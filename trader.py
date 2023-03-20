f7d51a43a93a260cc1440cca05541ed775de6f3c
Gompe <gomesp2002@gmail.com>
2023-03-20 07:06:38 +0100

    strategy pearls + bananas

--- trader.py
+++ trader.py
@@ -1,182 +1,214 @@
 from typing import Dict, List
 from datamodel import OrderDepth, TradingState, Order
 
-PEARL_PRICE = 10000
+import math
+
+# storing string as const to avoid typos
+SUBMISSION = "SUBMISSION"
+PEARLS = "PEARLS"
+BANANAS = "BANANAS"
+
+PRODUCTS = [
+    PEARLS,
+    BANANAS,
+]
+
+PEARL_PRICE = 10_000
+
 
 class Trader:
 
     def __init__(self) -> None:
-        # self.position = {
-        #     "PEARLS" : 0,
-        #     "BANANAS": 0
-        # }
-        self.pearl_position_limit = 20
 
-        self.last_orders = {
-            'BANANAS': {
-                'BID' : set(),
-                'ASK' : set()
-            },
-            'PEARLS' : {
-                'BID' : set(),
-                'ASK' : set()
-            }
+        self.position_limit = {
+            PEARLS : 20,
+            BANANAS : 20,
         }
 
-        self.pnl = 0
+        self.round = 0
+
+        # Values to compute pnl
+        self.cash = 0
+        # positions can be obtained from state.position
+        
+        # self.past_prices keeps the list of all past prices
+        self.past_prices = dict()
+        for product in PRODUCTS:
+            self.past_prices[product] = []
+
+        # self.ema_prices keeps an exponential moving average of prices
+        self.ema_prices = dict()
+        for product in PRODUCTS:
+            self.ema_prices[product] = None
+
+        self.ema_param = 0.5
+
+
+    # utils
+    def get_position(self, product, state):
+        return state.position.get(product, 0)    
+
+    def get_mid_price(self, product, state):
+        if product not in state.order_depths:
+            return None
+
+        market_bids = state.order_depths[product].buy_orders
+        if len(market_bids) == 0:
+            # There are no bid orders in the market (midprice undefined)
+            return None
+        
+        market_asks = state.order_depths[product].sell_orders
+        if len(market_asks) == 0:
+            # There are no bid orders in the market (mid_price undefined)
+            return None
+        
+        best_bid = max(market_bids)
+        best_ask = min(market_asks)
+        return (best_bid + best_ask)/2
+
+    def get_value_on_product(self, product, state):
+        """
+        Returns the amount of MONEY currently held on the product.  
+        """
+        return self.get_position(product, state) * self.get_mid_price(product, state)
+            
+    def update_pnl(self, state):
+        """
+        Updates the pnl.
+        """
+        def update_cash():
+            # Update cash
+            for product in state.own_trades:
+                for trade in state.own_trades[product]:
+                    if trade.timestamp != state.timestamp - 100:
+                        # Trade was already analyzed
+                        continue
+
+                    if trade.buyer == SUBMISSION:
+                        self.cash -= trade.quantity * trade.price
+                    if trade.seller == SUBMISSION:
+                        self.cash += trade.quantity * trade.price
+        
+        def get_value_on_positions():
+            value = 0
+            for product in state.position:
+                value += self.get_value_on_product(product, state)
+            return value
+        
+        # Update cash
+        update_cash()
+        return self.cash + get_value_on_positions()
+
+    def update_ema_prices(self, state):
+        """
+        Update the exponential moving average of the prices of each product.
+        """
+        for product in PRODUCTS:
+            mid_price = self.get_mid_price(product, state)
+            if mid_price is None:
+                continue
+
+            # Update ema price
+            if self.ema_prices[product] is None:
+                self.ema_prices[product] = mid_price
+            else:
+                self.ema_prices[product] = self.ema_param * mid_price + (1-self.ema_param) * self.ema_prices[product]
+
+
+    # Algorithm logic
+    def pearls_strategy(self, state):
+        """
+        Returns a list of orders with trades of pearls.
+
+        Comment: Mudar depois. Separar estrategia por produto assume que
+        cada produto eh tradado independentemente
+        """
+
+        position_pearls = self.get_position(PEARLS, state)
+
+        bid_volume = self.position_limit[PEARLS] - position_pearls
+        ask_volume = - self.position_limit[PEARLS] - position_pearls
+
+        orders = []
+        orders.append(Order(PEARLS, PEARL_PRICE - 1, bid_volume))
+        orders.append(Order(PEARLS, PEARL_PRICE + 1, ask_volume))
+
+        return orders
+
+    def bananas_strategy(self, state):
+        """
+        Returns a list of orders with trades of bananas.
+
+        Comment: Mudar depois. Separar estrategia por produto assume que
+        cada produto eh tradado independentemente
+        """
+
+        position_bananas = self.get_position(BANANAS, state)
+
+        bid_volume = self.position_limit[BANANAS] - position_bananas
+        ask_volume = - self.position_limit[BANANAS] - position_bananas
+
+        orders = []
+
+        if position_bananas == 0:
+            # Not long nor short
+            orders.append(Order(BANANAS, math.floor(self.ema_prices[BANANAS] - 1), bid_volume))
+            orders.append(Order(BANANAS, math.ceil(self.ema_prices[BANANAS] + 1), ask_volume))
+        
+        if position_bananas > 0:
+            # Long position
+            orders.append(Order(BANANAS, math.floor(self.ema_prices[BANANAS] - 2), bid_volume))
+            orders.append(Order(BANANAS, math.ceil(self.ema_prices[BANANAS]), ask_volume))
+
+        if position_bananas < 0:
+            # Short position
+            orders.append(Order(BANANAS, math.floor(self.ema_prices[BANANAS]), bid_volume))
+            orders.append(Order(BANANAS, math.ceil(self.ema_prices[BANANAS] + 2), ask_volume))
+
+        return orders
+
 
     def run(self, state: TradingState) -> Dict[str, List[Order]]:
         """
         Only method required. It takes all buy and sell orders for all symbols as an input,
         and outputs a list of orders to be sent
         """
+
+        self.round += 1
+        pnl = self.update_pnl(state)
+        self.update_ema_prices(state)
+
+        print(f"Log round {self.round}")
+
+        print("TRADES:")
+        for product in state.own_trades:
+            for trade in state.own_trades[product]:
+                if trade.timestamp == state.timestamp - 100:
+                    print(trade)
+
+        print(f"\tCash {self.cash}")
+        for product in PRODUCTS:
+            print(f"\tProduct {product}, Position {self.get_position(product, state)}, Midprice {self.get_mid_price(product, state)}, Value {self.get_value_on_product(product, state)}, EMA {self.ema_prices[product]}")
+        print(f"\tPnL {pnl}")
+        
+
         # Initialize the method output dict as an empty dict
         result = {}
-        
-        position_pearls = state.position['PEARLS'] if 'PEARLS' in state.position else 0
-        position_bananas = state.position['BANANAS'] if 'BANANAS' in state.position else 0
 
-        pnl = 0
-        
-        if 'PEARLS' in state.own_trades.keys():
-            total_trades = state.own_trades['PEARLS']
+        # PEARL STRATEGY
+        try:
+            result[PEARLS] = self.pearls_strategy(state)
+        except Exception as e:
+            print("Error in pearls strategy")
+            print(e)
 
-            if 'PEARLS' in state.order_depths.keys():
-                market_trades_pearls_bid = state.order_depths['PEARLS'].buy_orders
-                market_trades_pearls_ask = state.order_depths['PEARLS'].sell_orders
+        # BANANA STRATEGY
+        try:
+            result[BANANAS] = self.bananas_strategy(state)
+        except Exception as e:
+            print("Error in bananas strategy")
+            print(e)
 
-                best_ask = min(market_trades_pearls_ask.keys())
-                best_bid = max(market_trades_pearls_bid.keys())
+        print("+---------------------------------+")
 
-                mid_price_pearl = (best_ask + best_bid)/2
-
-                print(f"Mid price: {mid_price_pearl}")
-                for trade in total_trades:
-                    if trade.timestamp == state.timestamp - 100:
-                        if trade.price < PEARL_PRICE:
-                            print(f"trade: {trade}")
-                            pnl += trade.quantity * (PEARL_PRICE - trade.price)
-                        else:
-                            print(f"trade: {trade}")
-                            pnl -= trade.quantity * (PEARL_PRICE - trade.price)
-
-            # self.last_orders['PEARLS']['BID'] = set()
-            # self.last_orders['PEARLS']['ASK'] = set()
-
-        if 'BANANAS' in state.own_trades.keys():
-            total_trades = state.own_trades['BANANAS']
-
-            if 'BANANAS' in state.order_depths.keys():
-                market_trades_bananas_bid = state.order_depths['BANANAS'].buy_orders
-                market_trades_bananas_ask = state.order_depths['BANANAS'].sell_orders
-
-                best_bid = max(market_trades_bananas_bid.keys())
-                best_ask = min(market_trades_bananas_ask.keys())
-
-                mid_price_bananas = (best_ask + best_bid)/2
-
-                print(f"Mid price: {mid_price_bananas}")
-                for trade in total_trades:
-                    if trade.timestamp == state.timestamp - 100:
-                        if trade.price in self.last_orders['BANANAS']['BID']: # Must correct as bid might be below bid price sent (!!!)
-                            print(f"trade: {trade}")
-                            pnl += trade.quantity * (mid_price_bananas - trade.price)
-                        else:
-                            print(f"trade: {trade}")
-                            pnl -= trade.quantity * (mid_price_bananas - trade.price)
-
-            self.last_orders['BANANAS']['BID'] = set()
-            self.last_orders['BANANAS']['ASK'] = set()
-
-        self.pnl += pnl
-
-        print(f"PNL = {self.pnl}")
-
-        # Iterate over all the keys (the available products) contained in the order depths
-
-        for product in state.order_depths.keys():
-            if product == 'PEARLS':
-                order_depth: OrderDepth = state.order_depths[product]
-                orders: list[Order] = []
-
-                best_bid = PEARL_PRICE - 1
-                bid_volume = self.pearl_position_limit - position_pearls
-                orders.append(Order(product, best_bid, bid_volume))
-
-                best_ask = PEARL_PRICE + 1
-                ask_volume = - self.pearl_position_limit - position_pearls
-                orders.append(Order(product, best_ask, ask_volume))
-
-                # if len(order_depth.buy_orders) > 0:
-                #     best_bid = max(order_depth.buy_orders.keys()) + 1
-                #     best_bid = PEARL_PRICE - 1
-                #     if best_bid < PEARL_PRICE:
-                #         bid_volume = self.pearl_position_limit - position_pearls
-                #         print(f"Inserting bid order {Order(product, best_bid, bid_volume)}")
-                #         orders.append(Order(product, best_bid, bid_volume))
-                #         self.last_orders['PEARLS']['BID'].add(best_bid)
-                # if len(order_depth.sell_orders) > 0:
-                #     best_ask = min(order_depth.sell_orders.keys()) - 1
-                #     best_ask = PEARL_PRICE + 1
-                #     if best_ask > PEARL_PRICE:
-                #         ask_volume = -self.pearl_position_limit - position_pearls
-                #         print(f"Inserting ask order {Order(product, best_ask, ask_volume)}")
-                #         orders.append(Order(product, best_ask, ask_volume))
-                #         self.last_orders['PEARLS']['ASK'].add(best_ask)
-
-                result[product] = orders
-
-        return result
-
-        for product in state.order_depths.keys():
-
-            # Check if the current product is the 'PEARLS' product, only then run the order logic
-            if product == 'PEARLS':
-
-                # Retrieve the Order Depth containing all the market BUY and SELL orders for PEARLS
-                order_depth: OrderDepth = state.order_depths[product]
-
-                # Initialize the list of Orders to be sent as an empty list
-                orders: list[Order] = []
-
-                # Define a fair value for the PEARLS.
-                # Note that this value of 1 is just a dummy value, you should likely change it!
-                acceptable_price = 1
-
-                # If statement checks if there are any SELL orders in the PEARLS market
-                if len(order_depth.sell_orders) > 0:
-
-                    # Sort all the available sell orders by their price,
-                    # and select only the sell order with the lowest price
-                    best_ask = min(order_depth.sell_orders.keys())
-                    best_ask_volume = order_depth.sell_orders[best_ask]
-
-                    # Check if the lowest ask (sell order) is lower than the above defined fair value
-                    if best_ask < acceptable_price:
-
-                        # In case the lowest ask is lower than our fair value,
-                        # This presents an opportunity for us to buy cheaply
-                        # The code below therefore sends a BUY order at the price level of the ask,
-                        # with the same quantity
-                        # We expect this order to trade with the sell order
-                        # print("BUY", str(-best_ask_volume) + "x", best_ask)
-                        orders.append(Order(product, best_ask, -best_ask_volume))
-
-                # The below code block is similar to the one above,
-                # the difference is that it finds the highest bid (buy order)
-                # If the price of the order is higher than the fair value
-                # This is an opportunity to sell at a premium
-                if len(order_depth.buy_orders) != 0:
-                    best_bid = max(order_depth.buy_orders.keys())
-                    best_bid_volume = order_depth.buy_orders[best_bid]
-                    if best_bid > acceptable_price:
-                        # print("SELL", str(best_bid_volume) + "x", best_bid)
-                        orders.append(Order(product, best_bid, -best_bid_volume))
-
-                # Add all the above orders to the result dict
-                result[product] = orders
-
-                # Return the dict of orders
-                # These possibly contain buy or sell orders for PEARLS
-                # Depending on the logic above
         return result
