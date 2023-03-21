209f7b805b87612f24090c3cfa763905f9b0f42a
Gompe <gomesp2002@gmail.com>
2023-03-21 09:18:38 +0100

    ok

--- round2_trader.py
+++ round2_trader.py
@@ -25,19 +25,46 @@ DEFAULT_PRICES = {
     PINA_COLADAS : 15_000
 }
 
+POSITION_LIMITS = {
+    PEARLS : 20,
+    BANANAS : 20,
+    COCONUTS : 600,
+    PINA_COLADAS : 300,
+}
+
 MEAN_SPREAD = DEFAULT_PRICES[PINA_COLADAS] - DEFAULT_PRICES[COCONUTS]
 MEAN_SPREAD_STD = 10
+
+# Coeff for LinReg Model 
+# PINA_COLADA = COEF_1 * COCONUT + COEF_0
+COEF_1 = 1.551
+COEF_0 = 2593
+
+
+def fast_order(product, price, quantity):
+    """
+    Creates an order of the desired product, for the desired quantity. The
+    price is rounded to an integer.
+
+    If the order is a buy order, price is rounded up. Otherwise price is 
+    rounded down.
+
+    The idea behind this order is to get the order executed quickly by rounding
+    the price appropriately.
+    """
+
+    if quantity < 0:
+        price = math.floor(price)
+    else:
+        price = math.ceil(price)
+
+    return Order(product, price, quantity)
 
 class Trader:
 
     def __init__(self) -> None:
         
         print("Initializing Trader...")
-
-        self.position_limit = {
-            PEARLS : 20,
-            BANANAS : 20,
-        }
 
         self.round = 0
 
@@ -145,8 +172,8 @@ class Trader:
 
         position_pearls = self.get_position(PEARLS, state)
 
-        bid_volume = self.position_limit[PEARLS] - position_pearls
-        ask_volume = - self.position_limit[PEARLS] - position_pearls
+        bid_volume = POSITION_LIMITS[PEARLS] - position_pearls
+        ask_volume = - POSITION_LIMITS[PEARLS] - position_pearls
 
         orders = []
         orders.append(Order(PEARLS, DEFAULT_PRICES[PEARLS] - 1, bid_volume))
@@ -164,8 +191,8 @@ class Trader:
 
         position_bananas = self.get_position(BANANAS, state)
 
-        bid_volume = self.position_limit[BANANAS] - position_bananas
-        ask_volume = - self.position_limit[BANANAS] - position_bananas
+        bid_volume = POSITION_LIMITS[BANANAS] - position_bananas
+        ask_volume = - POSITION_LIMITS[BANANAS] - position_bananas
 
         orders = []
 
@@ -221,6 +248,61 @@ class Trader:
         return orders_coconuts, orders_pina_coladas
 
 
+    def coconut_pina_colada_linreg_strategy(self, state : TradingState):
+        # First strategy - no attempt to stay hedged
+
+        # Acochambrada:
+        POSITION_LIMITS[COCONUTS] = int(POSITION_LIMITS[PINA_COLADAS] * COEF_1)
+
+        orders_coconuts = []
+        orders_pina_coladas = []
+        
+        mid_price_coconuts = self.get_mid_price(COCONUTS, state)
+        mid_price_pina_coladas = self.get_mid_price(PINA_COLADAS, state)
+
+        coconuts_position = self.get_position(COCONUTS, state)
+        pina_coladas_position = self.get_position(PINA_COLADAS, state)
+
+        # Predicted "fair" pina colada price
+        fair_price = mid_price_coconuts * COEF_1 + COEF_0
+
+        # Margin of safety
+        MARGIN = 5
+
+        print("PINA-COCONUT STATS:")
+        print("COCONUT MIDPRICE", mid_price_coconuts)
+        print("PINA_COLADA MIDPRICE", mid_price_pina_coladas)
+        print("FAIR PRICE", fair_price)
+        
+        if fair_price > mid_price_pina_coladas + MARGIN:
+            print("Signal - pina is cheap")
+            # Pina coladas are cheap -> buy pina and sell coconuts
+            size_coconut = -POSITION_LIMITS[COCONUTS] - coconuts_position
+            size_pina_coladas = POSITION_LIMITS[PINA_COLADAS] - pina_coladas_position
+
+            orders_coconuts.append(fast_order(COCONUTS, mid_price_coconuts-0.5, size_coconut))
+            orders_pina_coladas.append(fast_order(PINA_COLADAS, mid_price_pina_coladas+0.5, size_pina_coladas))
+
+        elif fair_price < mid_price_pina_coladas - MARGIN:
+            print("Signal - pina is expensive")
+            # Pina coladas are expensive -> sell pina and buy coconuts
+            size_coconut = POSITION_LIMITS[COCONUTS] - coconuts_position
+            size_pina_coladas = -POSITION_LIMITS[PINA_COLADAS] - pina_coladas_position
+
+            orders_coconuts.append(fast_order(COCONUTS, mid_price_coconuts+0.5, size_coconut))
+            orders_pina_coladas.append(fast_order(PINA_COLADAS, mid_price_pina_coladas-0.5, size_pina_coladas))
+        
+        else:
+            print("Signal - pina is ok")
+            # Price relation seems fair - time to get hedged
+            orders_coconuts.append(fast_order(COCONUTS, mid_price_coconuts, -coconuts_position))
+            orders_pina_coladas.append(fast_order(PINA_COLADAS, mid_price_pina_coladas, -pina_coladas_position))
+
+        print("Orders submitted pina-coconut")
+        print(orders_coconuts)
+        print(orders_pina_coladas)
+
+        return orders_coconuts, orders_pina_coladas
 
     def run(self, state: TradingState) -> Dict[str, List[Order]]:
         """
@@ -265,7 +347,7 @@ class Trader:
 
         # COCONUTS AND PINA COLADAS STRATEGY
         try:
-            result[COCONUTS], result[PINA_COLADAS] = self.coconuts_pina_coladas_strategy(state)
+            result[COCONUTS], result[PINA_COLADAS] = self.coconut_pina_colada_linreg_strategy(state)
         except Exception as e:
             print("Error in coconuts and pina coladas strategy")
             print(e)
